012ebccd3786b95a5d000f9b2dfc7c9e5cd3eb14
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-24 00:14:59 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,10 +4,5 @@
 int main() {
     
     using namespace panagiotis;
-
-    /*std::cout << MyStruct<false,false>::Power_v<-5, -16><<'\n';
-    std::cout << (1.0 / 3125);*/
-    std::cout << Power_of_integer_nums<-2, -2>()<<'\n';
-   
-   
+    
 }
