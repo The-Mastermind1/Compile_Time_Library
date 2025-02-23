d2ec84e5d856d42d9593615e7bd4e843edb8f4f0
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-23 19:09:03 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,13 @@
 #include"Header.h"
 
+
 int main() {
     
     using namespace panagiotis;
 
-    //std::cout << Power_v<(size_t)5,(size_t)15>;
-    std::cout << Power_of_nums<(long long )-5, (long long )-2>();
-    
-    return 0;
-
-
+    /*std::cout << MyStruct<false,false>::Power_v<-5, -16><<'\n';
+    std::cout << (1.0 / 3125);*/
+    std::cout << Power_of_integer_nums<-2, -2>()<<'\n';
+   
+   
 }
