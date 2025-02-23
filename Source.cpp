a2dc8acd888a963c9dd48138dc779c546beaa7f2
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-23 13:09:26 +0200

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include"Header.h"
+
+int main() {
+    
+    using namespace panagiotis;
+
+    //std::cout << Power_v<(size_t)5,(size_t)15>;
+    std::cout << Power_of_nums<(long long )-5, (long long )-2>();
+    
+    return 0;
+
+
+}
