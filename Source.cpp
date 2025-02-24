5a672bc878cc0bac74a9940bbc7824fe255bbde1
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-24 15:42:38 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,12 @@
 #include"Header.h"
-
-
+using namespace panagiotis;
+constexpr int kati(int n) {
+    return n*n;
+}
 int main() {
+    std::array<int, Finobacci_V<5>>b;
+    std::cout<<b.size();
     
-    using namespace panagiotis;
+   
     
 }
