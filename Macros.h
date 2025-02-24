6637a8b809761c3121583edc2da6ca7470524cc5
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-24 15:42:25 +0200

    Update Macros.h

--- Macros.h
+++ Macros.h
@@ -3,6 +3,7 @@
 #include<string>
 #include<utility>
 #include<exception>
+#include<cmath>
 #define _NODISCARD [[nodiscard]]
 
 #define _PANAGIOTIS_BEGIN namespace panagiotis{
@@ -20,6 +21,15 @@ inline constexpr bool is_decimal_v = std::disjunction_v<std::is_same<_Ty, float>
 template<typename _Ty>
 struct is_decimal :std::bool_constant<is_decimal_v<_Ty>> {};//tag dispatching must support it
 
+ template <class _Ty>
+constexpr bool is_integer_v = std::_Is_any_of_v<_Ty, bool, char, signed char, unsigned char, wchar_t,
+#ifdef __cpp_char8_t
+    char8_t,
+#endif // defined(__cpp_char8_t)
+    char16_t, char32_t, short, unsigned short, int, unsigned int, long, unsigned long, long long, unsigned long long>;
+
+ template <class _Ty>
+struct is_integer : std::bool_constant<is_integer_v<_Ty>> {};
 
 template<bool condition1,bool condition2>
 struct MyStruct;
@@ -28,51 +38,51 @@ struct MyStruct;
 template <>
 struct MyStruct<true,true> {
 private:
-    template<size_t x,size_t  n>
+    template<std::size_t x, std::size_t  n>
     struct Power {
- 	  inline static constexpr size_t value = x * Power<x, n-1>::value;
+ 	  inline static constexpr std::size_t value = x * Power<x, n-1>::value;
     };
-    template<size_t x>
+    template<std::size_t x>
     struct Power<x, 1> {
- 	  inline static constexpr size_t value = x;
+ 	  inline static constexpr std::size_t value = x;
     };
 public:
  
- template<size_t x, size_t n>
- static constexpr size_t Power_v = Power<x, n>::value;
+ template<std::size_t x, std::size_t n>
+ static constexpr std::size_t Power_v = Power<x, n>::value;
 };
 
 // Specialization for false
 template <>
 struct MyStruct<false,true> {
 private:
-    template<long long x, size_t  n>
+    template<long long x, std::size_t  n>
     struct Power {
         inline static constexpr long long value = x * Power<x, n - 1>::value;
     };
-    template<size_t x>
+    template<std::size_t x>
     struct Power<x, 1> {
         inline static constexpr long long  value = x;
     };
 public:
 
-    template<long long x,size_t n>
+    template<long long x, std::size_t n>
     static constexpr long long Power_v = Power<x, n>::value;
 };
 template <>
 struct MyStruct<true,false> {
 private:
-    template<size_t x,long long  n>
+    template<std::size_t x,long long  n>
     struct Power {
         inline static constexpr double value =  (x * Power<x, n - 1>::value);
     };
-    template<size_t x>
+    template<std::size_t x>
     struct Power<x, 1> {
         inline static constexpr double  value =x;
     };
 public:
 
-    template<size_t x, long long n>
+    template<std::size_t x, long long n>
     static constexpr double Power_v =1.0/ Power<x, -n>::value;
 };
 
