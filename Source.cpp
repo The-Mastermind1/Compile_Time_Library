e8db5d37435de936a602d9b72548eac48cf30b53
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-03-03 02:23:59 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,23 @@
 #include"Header.h"
 #include <iostream>
 #include <numeric>
-#include <array>
+
 using namespace panagiotis;
 
 
 
 
-
 int main() {
-    constexpr static std::array<int, 5>numbers1={ 1,2,3,4,5 };
-     
-    constexpr static std::array<int, 5>numbers2 = { 6,7,8,9,10 };
-    constexpr static std::array<int, 5> a=Array_Algorithms::doaddition<int,5>(numbers1.cbegin(), numbers2.cbegin());
-    
-    
-    //constexpr int result = std::accumulate(numbers.begin(), numbers.end(), 0);
-    //std::cout << "Sum: " << result << std::endl;
+	      // Prints type name with the compiler's internal representation
+       
+        
+    constexpr auto reversed = make_reversed_string("compile");
+
+         for (char c : reversed) {
+             std::cout << c;
+         }
+          std::cout << '\n';  // Output: elipmoc
+       
+	 
+	
 }
