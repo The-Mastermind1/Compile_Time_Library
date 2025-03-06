f11801e233f6568715229b944771916b64b249f2
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-03-06 14:02:56 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,26 +2,13 @@
 #include <iostream>
 #include <numeric>
 #include<tuple>
+#include<variant>
 using namespace panagiotis;
-
-void kati(std::array<std::array<int, 4>, 5>::iterator it1) {
-
-}
-
 
 
 int main() {
-	//constexpr static std::array<std::array<int, 5>, 4>b = {};
-	//constexpr bool a=Array_Algorithms::Is_NxN_Matrix(b);
-	//std::cout << a << '\n';
-	 std::array<std::array<int, 5>, 5>b{};
-	 bool a= Array_Algorithms::Is_NxN(b).value;
-	std::cout << a << '\n';
+	std::cout<<Min<5, 6, 7, 0>::value;
 	
-	
-	 int c[5][5] = {};
-	 bool d = Array_Algorithms::Is_NxN(c).value;
-	std::cout << d << '\n';
 
 	
 	
