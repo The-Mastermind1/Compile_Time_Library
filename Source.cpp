e37968ef09bec72ffbb7543758816d6860782105
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-03-08 20:11:09 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,13 +5,16 @@
 #include<variant>
 #include<bitset>
 using namespace panagiotis;
-template<typename t>
-void kati2() {
+struct a ;
+
+void kati(a(&n)[5][6]) {
 
 }
 int main() {
-	//Supports_Sizeof_V<int[5]>;
-	
-	std::cout<<typeid(Largest_Type<int[], float, double, size_t, int, std::string>::Type).name();
+
+	//int a[5][5];
+	//std::is_array_v<int[5][5]>;
+	Array_Algorithms::Is_NxN<a, 5, 5>::value;
+	//Is_Prime_v<"pateras">;
 	
 }
