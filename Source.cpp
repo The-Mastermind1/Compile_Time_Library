37b91d9e69588469a342640897c140898c0df3d0
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-03-08 19:27:28 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,15 @@
 #include <numeric>
 #include<tuple>
 #include<variant>
+#include<bitset>
 using namespace panagiotis;
+template<typename t>
+void kati2() {
 
-
+}
 int main() {
-	std::cout<<typeid(Largest_Type<double,float,int,long double>::Type).name();
+	//Supports_Sizeof_V<int[5]>;
 	
-
-	
-	
-
-	 
+	std::cout<<typeid(Largest_Type<int[], float, double, size_t, int, std::string>::Type).name();
 	
 }
