2b4e578025d7a3c4a48b2ed5ef72b2c5fbda6f8b
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-03-15 23:54:32 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,11 +7,5 @@
 using namespace panagiotis;
 
 int main() {
-	
-     constexpr bool value=Is_Palindrome("10");
-	 std::size_t n = 2;
-	 
-	 //std::cout << value << '\n';
-	 //std::size_t n = 5;
-	 //std::cout<<(n - 6);
+
 }
